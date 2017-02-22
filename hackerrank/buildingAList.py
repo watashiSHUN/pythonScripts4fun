3c7adae3cd9d9fa82e95dd2e1f70893064ed4569
ShunXian Cai <shucai@microsoft.com>
2017-02-21 23:26:32 -0800

    use dictionary instead of for loop to search for last index

--- hackerrank/buildingAList.py
+++ hackerrank/buildingAList.py
@@ -2,35 +2,25 @@
 
 for _ in range(int(input())):
     n = int(input())
+    count = 2**n-1 # size of a powerset, does not print the emptyset
     string = list(input())
     string.sort() # alphabetical order
-    lastLetter = string[-1] # all unique
-    lastLetterIndexInPrevious = 0
-    init = [0]*n
-    init[0] = 1
-    previousIndex = init
+    dictionary = {} # unique letters, each letter correspond to its index
+    for index,value in enumerate(string):
+        dictionary[value] = index
     previousOutput = list(string[0])
 
-    while not (len(previousOutput) == 1 and previousOutput[0] == lastLetter):
+    while count != 1:
+        count -= 1
         print("".join(previousOutput))
         # produce next based on previous
-        if lastLetterIndexInPrevious != (n-1):
+        lastLetterIndexInPrevious = dictionary[previousOutput[-1]]
+        if  lastLetterIndexInPrevious != n-1:
             # append next available letter
-            lastLetterIndexInPrevious += 1
-            previousOutput.append(string[lastLetterIndexInPrevious])
-            previousIndex[lastLetterIndexInPrevious] = 1
+            previousOutput.append(string[lastLetterIndexInPrevious+1])
         else:
-            # goes back and find the last no zero index, replace it with zero and move it back by 1
-            previousIndex[-1] = 0
             del previousOutput[-1]
-            for i in range(1,n):
-                inspectIndex = lastLetterIndexInPrevious-i; # lastLetterIndexInPrevious == n-1
-                if previousIndex[inspectIndex]:
-                    lastLetterIndexInPrevious = inspectIndex+1
-                    previousIndex[inspectIndex] = 0
-                    previousIndex[lastLetterIndexInPrevious] = 1
-                    del previousOutput[-1]
-                    previousOutput.append(string[lastLetterIndexInPrevious])
-                    break
+            lastNonEndLetterIndex = dictionary[previousOutput[-1]]
+            previousOutput[-1] = string[lastNonEndLetterIndex+1]
 
     print(string[-1])
