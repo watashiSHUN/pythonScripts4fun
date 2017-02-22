e7b3a79722d441f0f16155c389e8c52d7855cee7
ShunXian Cai <shucai@microsoft.com>
2017-02-21 23:58:44 -0800

    add a single nested for loop to produce powerset

--- hackerrank/buildingAList.py
+++ hackerrank/buildingAList.py
@@ -24,3 +24,19 @@ for _ in range(int(input())):
             previousOutput[-1] = string[lastNonEndLetterIndex+1]
 
     print(string[-1])
+
+
+# test to write nested for loops
+testIn = "abcde"
+n = len(testIn)
+for i in range(n):
+    # always represent the first letter in the output
+    print(testIn[i])
+    for j in range(i+1,n):
+        print(testIn[i],testIn[j],sep="")
+        for k in range(j+1,n):
+            print(testIn[i],testIn[j],testIn[k],sep="")
+            for l in range(k+1,n):
+                print(testIn[i],testIn[j],testIn[k],testIn[l],sep="")
+                for m in range(l+1,n):
+                    print(testIn[i],testIn[j],testIn[k],testIn[l],testIn[m],sep="")
