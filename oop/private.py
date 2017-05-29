d769d75e1588097c729c7f03a866a39f367515a2
shun <mike53669@gmail.com>
2017-05-29 14:54:22 -0700

    use data-descriptor to prevent instance.__dict__ override

--- oop/private.py
+++ oop/private.py
@@ -2,27 +2,37 @@
 class PrivateProperty:
     def __init__(self):
         self.values = {}
-    def __get__(self,instance,tmp): # readonly property
+    def __get__(self,instance,type): # readonly property
         pass
+    def __set__(self,instance,value):
+        pass                        # data descriptor, overide __dict__ when using dot
     def set(self,instance,value):
         self.values[instance] = value
     def get(self,instance):
         return self.values[instance]
 
-
 class MyClass:
     p1 = PrivateProperty()
     def __init__(self,val):
+        # if MyClass.p1 will invoke type.__getattribute__
         MyClass.__dict__['p1'].set(self,val)
     def printPrivateProperty(self):
-        print(MyClass.__dict__['p1'].get(self))
+        return MyClass.__dict__['p1'].get(self)
+
+def debugF (list_of_statements):
+    for log in list_of_statements:
+        try:
+            print(log,":",eval(log))
+        except:
+            # if not a statment, no output
+            exec(log)
+            print("statement:",log)
 
 if __name__ == "__main__":
-    test = MyClass(123)
-    print(test.__dict__) # {} since we store data with class not with instance
-    print(test.p1) # None __get__ method of descriptor is invoked
-    test.printPrivateProperty() # 123
-    test.p1 = 456  # __set__ method of descriptor is invoked
-    print(test.p1)
-    test.printPrivateProperty()
-    print(MyClass.__dict__['p1'])
+    debugF(["test = MyClass(123)",
+    "test.__dict__",
+    "test.p1",
+    "test.printPrivateProperty()",
+    "test.p1 = 456",
+    "test.__dict__",
+    "test.printPrivateProperty()"])
