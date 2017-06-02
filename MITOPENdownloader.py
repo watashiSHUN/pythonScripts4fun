71d96b6c74daf9e317280d5792b09d8d9aa9b71a
shun <mike53669@gmail.com>
2017-06-01 23:52:38 -0700

    add CLI

--- MITOPENdownloader.py
+++ MITOPENdownloader.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import requests,bs4
+import requests,bs4,re,click
 from os import path
 from threading import Thread
 
@@ -25,31 +25,38 @@ class Video:
                 print("progress", written*100//self.length,"%\r",flush=True,end="")
             print("finished downloading",'"', self.title,'"', flush=True)
 
+
+class VideoStreamSite():
+    hostReg = re.compile(r'.*://[^/]*')
+    def __init__(self, url):
+        self.url = url
+        hostReg = self.hostReg.search(url)
+        if hostReg:
+            self.host = hostReg.group()
+        else:
+            raise ValueError("can not get host from URL")
+
+    def singleThreadDownload(self, destination):
+        res = requests.get(self.url)
+        res.raise_for_status()
+        mainPage = bs4.BeautifulSoup(res.text)
+
+        videoList = mainPage.select('div[class="medialisting"] > a')
+        for v in videoList:
+            videoTitle = v.get('title')
+            videoPlayUrl = self.host +  v.get('href')
+            videoRes = requests.get(videoPlayUrl)
+            videoPlayPage = bs4.BeautifulSoup(videoRes.text)
+            url = videoPlayPage.select('#vid_transcript li a')[1].get("href")
+            video = Video(url,videoTitle)
+            video.download(destination)
+
+@click.command()
+@click.option("--url", prompt="stream website url")
+@click.option("--output", prompt="output directory")
+def commandLine(url,output):
+    videoStream = VideoStreamSite(url)
+    videoStream.singleThreadDownload(output)
+
 if __name__ == "__main__":
-    # testUrl = "http://www.archive.org/download/MIT6.006F11/MIT6_006F11_lec01_300k.mp4"
-    # v = Video(testUrl,"Lecture 1: Algorithmic Thinking, Peak Finding")
-    # v1 = Video("http://www.archive.org/download/MIT6.006F11/MIT6_006F11_lec02_300k.mp4","Lecture 2: Models of Computation, Document Distance")
-    # t = Thread(target=v.download,args=("/home/watashishun/Downloads",))
-    # t1 = Thread(target=v1.download,args=("/home/watashishun/Downloads",))
-    # t.start()
-    # t1.start()
-
-    # for testing
-    destination = "/home/watashishun/Downloads"
-    host = "https://ocw.mit.edu"
-    url = "https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-006-introduction-to-algorithms-fall-2011/lecture-videos/"
-
-
-    res = requests.get(url)
-    res.raise_for_status()
-    mainPage = bs4.BeautifulSoup(res.text)
-
-    videoLs = mainPage.select('div[class="medialisting"] > a')
-    for v in videoLs:
-        videoTitle = v.get('title')
-        videoPlayUrl = host +  v.get('href')
-        videoRes = requests.get(videoPlayUrl)
-        videoPlayPage = bs4.BeautifulSoup(videoRes.text)
-        url = videoPlayPage.select('#vid_transcript li a')[1].get("href")
-        video = Video(url,videoTitle)
-        video.download(destination)
+    commandLine()
