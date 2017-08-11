5a392a7ec0e58cee30043a4e95d16521495e091a
ShunXian Cai <shucai@microsoft.com>
2017-08-10 18:56:24 -0700

    take a folder of zips instead of single zip

--- AminusB.py
+++ AminusB.py
@@ -1,18 +1,33 @@
  #! /usr/bin/env python3
 import sys,os
 from zipfile import ZipFile
+import zipfile
 zipA = sys.argv[1]
-zipB = sys.argv[2]
+zipBs = sys.argv[2] # this as a folder
 
+union_set = set()
+for f in os.listdir(zipBs):
+    f_full = os.path.join(zipBs,f)
+    if zipfile.is_zipfile(f_full):
+        with ZipFile(f_full,'r') as zipf:
+            union_set.update(set(zipf.namelist()))
+
+zipfileCommon = None
+zipfileDiff = None
 with ZipFile(zipA,'r') as zA:
     fdsA = zA.namelist()
-    with ZipFile(zipB,'r') as zB:
-        fdsB = set(zB.namelist())
-        with ZipFile("difference.zip",'w') as difference:
-            with ZipFile("common.zip",'w') as common:
-                for fd in fdsA:
-                    if not fd.endswith('/'): #zip standard 
-                        if fd in fdsB:
-                            common.writestr(fd,zA.read(fd))
-                        else:
-                            difference.writestr(fd,zA.read(fd))
+    for fd in fdsA:
+        if not fd.endswith('/'): #zip standard
+            if fd in union_set:
+                if not zipfileCommon:
+                    zipfileCommon = ZipFile("common.zip",'w')
+                zipfileCommon.writestr(fd,zA.read(fd))
+            else:
+                if not zipfileDiff:
+                    zipfileDiff = ZipFile("difference.zip",'w')
+                zipfileDiff.writestr(fd,zA.read(fd))
+
+if not zipfileCommon:
+    zipfileCommon.close()
+if not zipfileDiff:
+    zipfileDiff.close()
