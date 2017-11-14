3a2e30f29b6739e4e8defe65ab165dc428ad3505
ShunXian Cai <shucai@microsoft.com>
2017-11-13 16:36:25 -0800

    fix if a is not None == if a

--- AminusB.py
+++ AminusB.py
@@ -3,7 +3,7 @@ import sys,os
 from zipfile import ZipFile
 import zipfile
 zipA = sys.argv[1]
-zipBs = sys.argv[2] # this as a folder
+zipBs = sys.argv[2] # this is a folder
 
 union_set = set()
 for f in os.listdir(zipBs):
@@ -19,6 +19,7 @@ with ZipFile(zipA,'r') as zA:
     for fd in fdsA:
         if not fd.endswith('/'): #zip standard
             if fd in union_set:
+                print("find common" + fd)
                 if not zipfileCommon:
                     zipfileCommon = ZipFile("common.zip",'w')
                 zipfileCommon.writestr(fd,zA.read(fd))
@@ -27,7 +28,7 @@ with ZipFile(zipA,'r') as zA:
                     zipfileDiff = ZipFile("difference.zip",'w')
                 zipfileDiff.writestr(fd,zA.read(fd))
 
-if not zipfileCommon:
+if zipfileCommon:
     zipfileCommon.close()
-if not zipfileDiff:
+if zipfileDiff:
     zipfileDiff.close()
