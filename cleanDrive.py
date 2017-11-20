61cd0283795647463ffd525eea32b5ffc93297dd
ShunXian Cai <shucai@microsoft.com>
2017-11-19 22:35:19 -0800

    display directory information only

--- cleanDrive.py
+++ cleanDrive.py
@@ -33,37 +33,46 @@ class Directory:
         self.dirName = dirName
         self.size = size
 
-    def __str__(self,indent=0):
+    # print the whole directory structure, with option to ignore files
+    def __str__(self, indent=0, onlyDir=True):
         spaceIndent = '  '*indent
         fs = ''
         ds = ''
         dKeys = list(self.subDirs.keys())
-        fKeys = list(self.files.keys())
-        fKeys.sort(key=lambda s: self.files[s].size, reverse=True)
         dKeys.sort(key=lambda s: self.subDirs[s].size, reverse=True)
         for d in dKeys:
-            ds += self.subDirs[d].__str__(indent=indent+1)
-        for f in fKeys:
-            fs += self.files[f].__str__(indent=indent+1) + '\n'
-        return spaceIndent + self.dirName + " : " + str(self.size) + "\n" + ds + fs
-
-    def Lvls(self, levels, indent=0):
-        if(levels < 0):
-            return ''
-        spaceIndent = '  '*indent
-        ds = ''
-        fs = ''
-        dKeys = list(self.subDirs.keys())
-        dKeys.sort(key=lambda s: self.subDirs[s].size, reverse=True)
-        for d in dKeys:
-            ds += self.subDirs[d].Lvls(levels -1,indent=indent+1)
-        if(levels != 0):
-            #directoryOnly
+            ds += self.subDirs[d].__str__(indent=indent+1, onlyDir=onlyDir)
+        if not onlyDir:
             fKeys = list(self.files.keys())
             fKeys.sort(key=lambda s: self.files[s].size, reverse=True)
             for f in fKeys:
                 fs += self.files[f].__str__(indent=indent+1) + '\n'
         return spaceIndent + self.dirName + " : " + str(self.size) + "\n" + ds + fs
+
+    # print levels of directory, with option to ignore files
+    # level == 1, print what's in current directory
+    def Lvls(self, levels, indent=0, onlyDir=True):
+        if (levels < 0):
+            return ''
+        spaceIndent = '  '*indent
+        currentSummary = spaceIndent + self.dirName + " : " + str(self.size) + "\n"
+        if (levels == 0):
+            # return dir.summary
+            return currentSummary
+        # start expanding
+        ds = ''
+        fs = ''
+        dKeys = list(self.subDirs.keys())
+        dKeys.sort(key=lambda s: self.subDirs[s].size, reverse=True)
+        for d in dKeys:
+            ds += self.subDirs[d].Lvls(levels-1, indent=indent+1, onlyDir=onlyDir)
+        if not onlyDir:
+            fKeys = list(self.files.keys())
+            fKeys.sort(key=lambda s: self.files[s].size, reverse=True)
+            for f in fKeys:
+                fs += self.files[f].__str__(indent=indent+1) + '\n'
+
+        return currentSummary + ds + fs
 
 class File:
 
@@ -72,7 +81,8 @@ class File:
         self.fullPath = os.path.join(dirPath,fileName)
         try:
             self.size = Bytes(os.path.getsize(self.fullPath))
-        except:
+        except ex:
+            # path exceed 260 length limit
             self.size = Bytes(0)
             print("failed to retrieve " + self.fullPath)
 
@@ -87,12 +97,21 @@ def createDirectory(dirPath, subDirs, files):
     createdSubDirs = {}
     subDirSize = Bytes(0)
     # for debugging
-    # if len(subDirs) > len(globalStack):
-    #     print("error too many subDirs", subDirs, globalStack)
-    for i in range(len(subDirs)):
-        name, sub = globalStack.pop()
+    if len(subDirs) > len(globalStack):
+        test = set([name for name, sub in globalStack])
+        missing = []
+        for d in subDirs:
+            if d not in test:
+                missing.append(d)
+        print("error: missing subdirectories ", missing, "from directory", dirPath)
+    # My Pictures NOT IN My Documents
+    while globalStack:
+        name, sub = globalStack[-1] # equivalent of peek()
+        if name not in subDirs:
+            break
         createdSubDirs[name] = sub
         subDirSize += sub.size
+        globalStack.pop()
     # for debugging
     # for s in subDirs:
     #     if (s not in createdSubDirs):
@@ -103,10 +122,11 @@ def createDirectory(dirPath, subDirs, files):
         currentDirFiles[f] = newf
         subDirSize += newf.size
 
+    # XXX windows only
     dirName = dirPath.split("\\")[-1]
-    return (dirName, Directory(dirPath, subDirSize, createdSubDirs, currentDirFiles))
+    return (dirName, Directory(dirName, subDirSize, createdSubDirs, currentDirFiles))
 
-drive = "C:\\php"
+drive = "C:\\"
 print("processing \""+drive+"\"")
 globalStack = [] # tuple (directoryname, directory object)
 
